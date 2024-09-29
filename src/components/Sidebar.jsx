8babf449047c1622ca9cdfe4103cad8cb0147158
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-09-29 23:54:40 +0600

    second commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,10 +10,10 @@ const Sidebar = () => {
     <div className='bg-gray-300 text-gray-900 h-screen px-4 fixed w-full md:w-64 border-r border-gray-400'>
         <h1 className='text-2xl font-bold hidden md:block mt-4 text-center'>Admin</h1>
 
-        <ul className='flex flex-col mt-5 text-xl'>
-            <li className='flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer hover:bg-blue-600 hover:text-white'>
+        <ul className='flex flex-col mt-7 text-xl'>
+            <li className='flex items-center py-3 px-2 space-x-4 rounded cursor-pointer bg-blue-600 text-white'>
                 <RiAdminLine/>
-                <span className='hidden md:inline'>Dashboard</span>
+                <span className='hidden md:inline '>Dashboard</span>
             </li>
 
             <li className='flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer hover:bg-blue-600 hover:text-white'>
